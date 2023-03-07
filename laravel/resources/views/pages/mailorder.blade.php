ff3e3fdf0aa046556f5cdc4102e209798c5f33ed
Yasuhiro Fukuda <yfukuda827@outlook.jp>
2023-03-07 15:01:12 +0900

    #6 特定商取引法に関わる表記ページ追加

---
+++ laravel/resources/views/pages/mailorder.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.bootstrap')
+
+@section('content')
+<!-- Masthead-->
+<header class="masthead">
+    <div class="container">
+        <div class="masthead-subheading">世界で１つだけの商品 職人が織りなす技術美</div>
+        <div class="masthead-heading text-uppercase">わたしたちは最高の１点を作り上げます</div>
+    </div>
+</header>
+<section class="page-section">
+    <div class="container">
+        <h1>特定商取引法に関わる表記</h1>
+        <table class="table table-striped">
+            <tbody>
+                <tr>
+                    <th>販売業者</th>
+                    <td>株式会社Start Bootstrap</td>
+                </tr>
+                <tr>
+                    <th>代表責任者</th>
+                    <td>坂元 一平</td>
+                </tr>
+                <tr>
+                    <th>所在地</th>
+                    <td>146-0082 東京都大田区池上99-1</td>
+                </tr>
+                <tr>
+                    <th>電話番号</th>
+                    <td>03-0000-0000　（土日祝日をのぞく、8:00～17:00受付）</td>
+                </tr>
+                <tr>
+                    <th>サイトURL</th>
+                    <td>https://startbootstrap.com</td>
+                </tr>
+                <tr>
+                    <th>販売価格</th>
+                    <td>各商品の紹介ページに記載している価格とします。</td>
+                </tr>
+                <tr>
+                    <th>商品代金以外に必要な料金</th>
+                    <td>消費税、送料（北海道・沖縄1100円。その他地域一律990円。）</td>
+                </tr>
+                <tr>
+                    <th>引き渡し時期</th>
+                    <td>受注生産のため、ご発注後に生産して納入いたします。納品日は、別途連絡いたします。</td>
+                </tr>
+                <tr>
+                    <th>お支払方法とお支払い時期</th>
+                    <td>
+                        クレジットカード決済：ご注文時にお支払いが確定します。<br>
+                        代金引換：代金は商品お届け時、配送員に現金でお支払いください。<br>
+                        銀行振込：ご注文から3日以内に、指定銀行口座へお支払いください。
+                    </td>
+                </tr>
+                <tr>
+                    <th>返品・交換・キャンセルについて</th>
+                    <td>商品発送後の返品・交換・キャンセルには、基本的に対応しておりません。<br>
+                    商品に欠陥がある場合のみ交換が可能ですのでご連絡ください。</td>
+                </tr>
+                <tr>
+                    <th>返品期限</th>
+                    <td>商品出荷から◯日以内にご連絡ください。</td>
+                </tr>
+                <tr>
+                    <th>返品送料</th>
+                    <td>商品に欠陥がある場合は、弊社で負担いたします。<br>
+                    それ以外は、お客さまのご負担になります。</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</section>
+@endsection
